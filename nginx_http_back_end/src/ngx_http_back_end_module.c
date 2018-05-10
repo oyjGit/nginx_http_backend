287dbb444cf3dab7b59972ce45cd9efe200b6263
oyjGit <511244213@qq.com>
2018-05-10 13:51:25 +0800

    first commit

---
+++ nginx_http_back_end/src/ngx_http_back_end_module.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "ngx_module.h"
+#include "ngx_http_config.h"
+
+static ngx_command_t  ngx_http_commands[] = {
+	{ 
+		ngx_string("http_back_end"),
+		NGX_MAIN_CONF | NGX_CONF_BLOCK | NGX_CONF_NOARGS,
+		NULL,
+		0,
+		0,
+		NULL 
+	},
+
+	ngx_null_command
+};
+
+static ngx_http_module_t ngx_http_module_ctx = 
+{
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
+
+ngx_module_t ngx_http_back_end_module =
+{
+	
+	NULL,
+
+
+};
+
+
