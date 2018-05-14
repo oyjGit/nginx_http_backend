e37597dc00c3f5a1fc2172064e131f5d55f43da4
oyjGit <511244213@qq.com>
2018-05-14 17:26:20 +0800

    igonre openssl bin

--- nginx-master/build.sh
+++ nginx-master/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #如果加上openssl连接的时候出现找不到pthreaad函数，需要修改objs/Makefile,将phtread放到最后
-./auto/configure --add-module=../nginx_http_back_end --with-http_stub_status_module --with-http_ssl_module --with-openssl=../openssl-1.1.1-pre6
+./auto/configure --add-module=../nginx_http_back_end --with-debug --with-http_stub_status_module --with-http_ssl_module --with-openssl=../openssl-1.1.1-pre6
