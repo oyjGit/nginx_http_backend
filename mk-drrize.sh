28603e040f650060c0b4d94ac1b92dd738ac81b0
oyjGit <511244213@qq.com>
2018-06-22 11:52:17 +0800

    add drrizle

---
+++ mk-drrize.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#http://openresty.org/download/drizzle7-2011.07.21.tar.gz
+sudo apt-get install gperf
+sudo apt-get install intltool
+sudo apt-get install libprotoc-dev
+sudo apt-get install protobuf-compiler
+sudo apt-get install uuid-dev
+sudo apt-get install lib64readline6-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install libboost-program-options-dev
+./configure --without-server
+make libdrizzle-1.0
+sudo make install-libdrizzle-1.0
--- nginx-master/build.sh
+++ nginx-master/build.sh
@@ -4,4 +4,4 @@
 #./auto/configure --add-module=../nginx_http_back_end --with-debug --with-http_stub_status_module --with-http_ssl_module --with-openssl=../openssl-1.1.1-pre6
 
 #openssl链接系统中安装的openssl,ubuntu 需要apt-get install libssl-dev
-./auto/configure --add-module=../nginx_http_back_end --with-debug --with-http_stub_status_module --with-http_ssl_module --add-dynamic-module=../redis2-nginx-module-master --add-dynamic-module=../ngx_devel_kit --add-dynamic-module=../set-misc-nginx-module-master
+./auto/configure --add-module=../nginx_http_back_end --with-debug --with-http_stub_status_module --with-http_ssl_module --add-dynamic-module=../redis2-nginx-module-master --add-dynamic-module=../ngx_devel_kit --add-dynamic-module=../set-misc-nginx-module-master --add-dynamic-module=../drizzle-nginx-module-master --add-dynamic-module=../rds-json-nginx-module-master
